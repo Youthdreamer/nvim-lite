8c25b5316e51c77da05ffde203fac7411bd7ec88
youth <95559199+Youthdreamer@users.noreply.github.com>
2026-01-30 22:36:34 +0800

    删除错误注释

--- init.lua
+++ init.lua
@@ -92,7 +92,7 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-mini/mini.pick" }, -- 文件/缓冲区选择器
 	{ src = "https://github.com/nvim-mini/mini.files" }, -- 文件浏览器
 }, { load = false })
--- 插件延迟加载（在读取文件或创建新文件时加载）
+-- 其实这个没必要
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
 		-- mini.pick 配置
